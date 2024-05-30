81e57714840745b014c77e41a510b9e10aee5f71
Ricardo B. Marliere <ricardo@marliere.net>
2024-05-30 14:48:59 +0000

    quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let mut un: i32 = 2;
+    if quantity > 40 {
+        un = 1;
+    }
+    quantity * un
+}
 
 // Don't modify this function!
 #[test]
